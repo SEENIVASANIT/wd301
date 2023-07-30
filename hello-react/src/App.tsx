aeeec97a91f43054b74f9794e5f09b1df3abd1ba
SEENIVASANIT <hockeyseeni@gmail.com>
2023-07-30 16:58:37 +0530

    l2

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -1,46 +1,48 @@
+/* eslint-disable no-unused-vars */
 import React from "react";
 import TaskCard from "./TaskCard";
 
 function App() {
   return (
-    <div className="center">
-      <h1 className="flex p-4 font-bold text-4xl">Smarter Tasks</h1>
-      <p className="flex p-5 font-bold text-sm">
-        Project: Graduation FInal Year Project (Revamp College Website)
-      </p>
-      <div className="md:flex justify-center md:h-full">
-        <div className=" p-5 border border-gray-500 m-4 rounded-lg">
-          <h1 className="p-3 text-center">Pending</h1>
-          <TaskCard
-            title="Build the website with static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add blog"
-            dueDate="27nd Jan"
-            assigneeName="loght s"
-          />
-          <button className="">+ Add Task</button>
-        </div>
-        <div className="w-1/2 p-4 border border-gray-500 m-4 rounded-lg">
-          <h1 className="p-3 text-center">Done</h1>
-          <TaskCard
-            title="Design the mockup"
-            completedAtDate="10th jully"
-            assigneeName="seeni M"
-          />
-          <TaskCard
-            title="Get approval from principal"
-            completedAtDate="26th April"
-            assigneeName="jeeva S"
-          />
+    <>
+      <div className="mx-10 my-5">
+        <h1 className="text-2xl font-bold my-2 mx-32">Smarter Tasks</h1>
+        <p className="font-bold text-sm  mb-3">
+          Project: Graduation FInal Year Project (Revamp College Website)
+        </p>
+        <div className="flex justify-center">
+          <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 ">
+            <h1 className="text-2xl font-bold text-center pb-5">pending</h1>
+            <TaskCard
+              title="Build the website with static content"
+              dueDate="10th April"
+              assigneeName="Rohit S"
+            />
+            <TaskCard
+              title="Add blog"
+              dueDate="22nd March"
+              assigneeName="Rohit M"
+            />
+            <button className="bg-gray-200 px-5 rounded-md">+ New Task</button>
+          </div>
+
+          <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 p-5 ml-14 mr-32">
+            <h1 className="text-2xl font-bold bold text-center pb-5">done</h1>
+            <TaskCard
+              title="Design the mockup"
+              completedAtDate="10th April"
+              assigneeName="Rohit M"
+            />
+            <TaskCard
+              title="Get approval from principal"
+              completedAtDate="20th April"
+              assigneeName="Ajay S"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
-
-
 
 export default App;
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -2,30 +2,26 @@
 import React from "react";
 import "./TaskCard.css";
 
-interface TaskCard {
-  title: string;
+interface TaskCardProps {
+  title?: string;
   dueDate?: string;
   completedAtDate?: string;
-  assigneeName: string;
+  assigneeName?: string;
 }
 
-let TaskCard = ({
-  title,
-  dueDate,
-  completedAtDate,
-  assigneeName,
-}: TaskCard) => {
-  let date;
-  if (dueDate) {
-    date = `Due Date: ${dueDate}`;
-  } else {
-    date = `Completed on: ${completedAtDate}`;
+const TaskCard = (props: TaskCardProps) => {
+  let taskDate: string;
+  if (props.dueDate) {
+    taskDate = `Due date: ${props.dueDate}`;
+  }
+  if (props.completedAtDate) {
+    taskDate = `Completed on: ${props.completedAtDate}`;
   }
   return (
-    <div className="TaskItem">
-      <h2 className="text-xl font-bold">{title}</h2>
-      <p>{date}</p>
-      <p>Assignee: {assigneeName}</p>
+    <div className="TaskItem w-full p-5">
+      <h2 className="text-xl font-bold mb-2">{props.title}</h2>
+      <p>{taskDate}</p>
+      <p>Assignee: {props.assigneeName}</p>
     </div>
   );
 };
