d519ee455efacd25a6395b413995d12fd80944b1
SEENIVASAN_T <89895557+SEENIVASANIT@users.noreply.github.com>
2023-09-26 12:49:41 +0530

    l7

---
+++ smarter-tasks/src/pages/signup/SignupForm.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+import { API_ENDPOINT } from "../../config/constants";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+
+type Inputs = {
+  organisationName: string;
+  userName: string;
+  userEmail: string;
+  userPassword: string;
+};
+const SignupForm: React.FC = () => {
+  const useNavigation = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    const { organisationName, userName, userEmail, userPassword } = data;
+    try {
+      const response = await fetch(`${API_ENDPOINT}/organisations`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name: organisationName,
+          user_name: userName,
+          email: userEmail,
+          password: userPassword,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Sign-up failed");
+      }
+      console.log("Sign-up successful");
+      let userData = await response.json();
+      localStorage.setItem("authToken", userData.token);
+      localStorage.setItem("userData", JSON.stringify(userData.user));
+        useNavigation("/account");
+    } catch (error) {
+      console.error("Sign-up failed:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label className="block text-gray-700 font-semibold mb-2">
+          Organisation Name:
+        </label>
+        <input
+          type="text"
+          id="organisationName"
+          className={`w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.organisationName ? "border-red-500" : ""
+          }`}
+          {...register("organisationName", { required: true })}
+        />
+        {errors.organisationName && (
+          <span className="text-red-500 text-sm">This field is required</span>
+        )}
+      </div>
+      <div>
+        <label className="block text-gray-700 font-semibold mb-2">
+          Your Name:
+        </label>
+        <input
+          type="text"
+          id="userName"
+          className={`w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.userName ? "border-red-500" : ""
+          }`}
+          {...register("userName", { required: true })}
+        />
+        {errors.userName && (
+          <span className="text-red-500 text-sm">This field is required</span>
+        )}
+      </div>
+      <div>
+        <label className="block text-gray-700 font-semibold mb-2">Email:</label>
+        <input
+          type="email"
+          id="userEmail"
+          className={`w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.userEmail ? "border-red-500" : ""
+          }`}
+          {...register("userEmail", { required: true })}
+        />
+        {errors.userEmail && (
+          <span className="text-red-500 text-sm">This field is required</span>
+        )}
+      </div>
+      <div>
+        <label className="block text-gray-700 font-semibold mb-2">
+          Password:
+        </label>
+        <input
+          type="password"
+          id="userPassword"
+          className={`w-full border rounded-md py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-blue-500 focus:shadow-outline-blue ${
+            errors.userPassword ? "border-red-500" : ""
+          }`}
+          {...register("userPassword", { required: true })}
+        />
+        {errors.userPassword && (
+          <span className="text-red-500 text-sm">This field is required</span>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="w-full bg-gray-700 hover:bg-gray-800 text-white font-semibold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline-gray mt-4"
+      >
+        Sign up
+      </button>
+    </form>
+  );
+};
+
+export default SignupForm;
---
+++ smarter-tasks/src/vite-env.d.ts
@@ -0,0 +1,6 @@
+interface ImportMetaEnv {
+  readonly VITE_REACT_APP_API_ENDPOINT: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
